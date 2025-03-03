1e917407a792f2ee647ce469f157ccad10c6ac7b
Cierra <cierra362@gmail.com>
2025-03-03 13:08:19 -0500

    fixed tutorial photos

--- cs_major_tracker/src/client/Tutorial.tsx
+++ cs_major_tracker/src/client/Tutorial.tsx
@@ -25,23 +25,23 @@ function Tutorial () {
             
             <main className="min-h-screen bg-[#AC2B37] flex flex-col items-center">
                 <Typography variant="h1" className="text-center content-center py-6 mt-20 text-white">Tutorial</Typography><br />
-                <img src="/assets/tutorial/tutorial.gif" className="max-w-[60%]" /><br />
+                <img src="/tutorial/tutorial.gif" className="max-w-[60%]" /><br />
 
                 <Card className="my-6 w-[60%]">
                     <CardBody className="flex flex-col p-8 gap-2">
                         <Typography variant="h2" className="text-[#AC2B37]">Step By Step</Typography><br />
                         <hr className="mt-[-20px] mb-6"></hr>
                         <Typography>1. Open Workday and click on <em>Academics</em> in the sidebar on the right.</Typography>
-                        <img src="/assets/tutorial/t1.png" className="max-w-[60%] self-center"></img><br />
+                        <img src="/tutorial/t1.png" className="max-w-[60%] self-center"></img><br />
 
                         <Typography>2. Next, click on <em>View My Academic Record</em> in the sidebar on the right.</Typography>
-                        <img src="/assets/tutorial/t2.png" className="max-w-[60%] self-center"></img><br />
+                        <img src="/tutorial/t2.png" className="max-w-[60%] self-center"></img><br />
 
                         <Typography>3. Click on the <em>Export to Excel</em> button in the top right corner.</Typography>
-                        <img src="/assets/tutorial/t3.png" className="max-w-[70%] self-center"></img><br />
+                        <img src="/tutorial/t3.png" className="max-w-[70%] self-center"></img><br />
 
                         <Typography>4. A modal will appear prompting you to export the document. Click <em>Download</em>.</Typography>
-                        <img src="/assets/tutorial/t4.png" className="max-w-[50%] self-center"></img><br />
+                        <img src="/tutorial/t4.png" className="max-w-[50%] self-center"></img><br />
 
                         <Typography>The excel file should download to your computer shortly.</Typography>
                     </CardBody>
