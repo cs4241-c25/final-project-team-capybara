8dc36b099eb5a7ca541a620fe03a08f8989e1b06
Cierra <cierra362@gmail.com>
2025-03-04 04:18:38 -0500

    minor header changes

--- cs_major_tracker/src/client/Header.tsx
+++ cs_major_tracker/src/client/Header.tsx
@@ -1,5 +1,6 @@
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import { useNavigate } from 'react-router-dom';
+import { Button } from "@material-tailwind/react";
 
 function Header() {
     const navigate = useNavigate();
@@ -26,14 +27,16 @@ function Header() {
     return (        
         <header className="bg-wpi-red border-b-2 border-shadow-red p-4 flex items-center">
             <span onClick={openNav} className="hover:bg-shadow-red pl-[6px] py-2"><Bars3Icon className="h-5 w-5 mr-3 ml-1 fill-white"></Bars3Icon></span>
-            <img src="/wpi.png" alt="WPI Logo" className="h-12 w-auto ml-4" />
+            <a href="/main"><img src="/wpi.png" alt="WPI Logo" className="h-12 w-auto ml-4" /></a>
 
-            <button
+            <Button
+              variant="outlined"
+              color="white"
               onClick={handleLogout}
-              className="bg-white hover:bg-gray-2 text-black px-4 py-2 mx-4 rounded ml-auto"
+              className="hover:bg-shadow-red text-white px-4 py-2 mx-4 rounded ml-auto"
             >
               Logout
-            </button>
+            </Button>
         </header>
     );
 }
